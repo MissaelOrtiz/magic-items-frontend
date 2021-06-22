833c202a09b3ee053031ff42e3e9f6234adb24f5
Missael Ortiz <ortiz.missael80@gmail.com>
2021-06-22 16:15:34 -0700

    added working homepage with clickable cards

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import { getAllItems } from './utils'
+import { Link } from 'react-router-dom'
 
 export default class ListPage extends Component {
+    state = {
+        magicItems: []
+    }
+
+    componentDidMount = async () => {
+        const items = await getAllItems();
+        this.setState({ magicItems: items })
+    }
     render() {
         return (
-            <div>
-                LIST, HOME PAGE
+            <div className="item-container">
+                {
+                    this.state.magicItems.map(item => <Link to={`/magicItems/${item.id}`}>
+                    <div className="item-card">
+                        <p>{item.name}</p>
+                        <p>Type: {item.type}</p>
+                        <p>Level: {item.level}</p>
+                        <p>Effect: {item.effect}</p>
+                    </div>
+                    </Link>)
+                }
             </div>
         )
     }
